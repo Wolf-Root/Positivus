b67af6c2df42aecaa2d4582505b737a7ffc0a8f2
Wolf-Root <mobilemax363@gmail.com>
2026-05-03 19:09:15 +0100

    Add border to outline variant and change hover Color

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/80",
         outline:
-          "border-2 border-border bg-background shadow-xs hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground",
+          "border-2 border-border bg-background shadow-xs hover:bg-primary/80 hover:text-foreground focus:bg-primary/80 aria-expanded:bg-muted aria-expanded:text-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
         ghost: "hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground",
